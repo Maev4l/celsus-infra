87ef847fcc3a15d98f06bc1cba7f9d8ea381c6a7
Maev4l <maeval.nightingale@gmail.com>
2019-04-20 09:49:18 +0200

    Create IAM roles for Cognito Identity pool

--- terraform/aws/authn-authz.tf
+++ terraform/aws/authn-authz.tf
@@ -37,3 +37,133 @@ resource "aws_cognito_identity_pool" "identity_pool" {
     server_side_token_check = "true"
   }
 }
+
+data "template_file" "identity_pool_auth_role_policy" {
+  template = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "$${cognito_identity_pool_id}"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "authenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+
+  vars {
+    cognito_identity_pool_id = "${aws_cognito_identity_pool.identity_pool.id}"
+  }
+}
+
+resource "aws_iam_role" "identity_pool_auth_role" {
+  name = "celsus_identity_authenticated_role"
+
+  assume_role_policy = "${data.template_file.identity_pool_auth_role_policy.rendered}"
+
+  tags = "${local.tags}"
+}
+
+resource "aws_iam_role_policy" "authenticated" {
+  name = "celsus_identity_authenticated_policy"
+  role = "${aws_iam_role.identity_pool_auth_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "mobileanalytics:PutEvents",
+                "cognito-sync:*",
+                "cognito-identity:*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+data "template_file" "identity_pool_unauth_role_policy" {
+  template = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "cognito-identity.amazonaws.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "cognito-identity.amazonaws.com:aud": "$${cognito_identity_pool_id}"
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr": "unauthenticated"
+        }
+      }
+    }
+  ]
+}
+EOF
+
+  vars {
+    cognito_identity_pool_id = "${aws_cognito_identity_pool.identity_pool.id}"
+  }
+}
+
+resource "aws_iam_role" "identity_pool_unauth_role" {
+  name = "celsus_identity_unauthenticated_role"
+
+  assume_role_policy = "${data.template_file.identity_pool_unauth_role_policy.rendered}"
+
+  tags = "${local.tags}"
+}
+
+resource "aws_iam_role_policy" "unauthenticated" {
+  name = "celsus_identity_unauthenticated_policy"
+  role = "${aws_iam_role.identity_pool_unauth_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "mobileanalytics:PutEvents",
+                "cognito-sync:*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "identity_pool_roles_main" {
+  identity_pool_id = "${aws_cognito_identity_pool.identity_pool.id}"
+
+  roles = {
+    "authenticated"   = "${aws_iam_role.identity_pool_auth_role.arn}"
+    "unauthenticated" = "${aws_iam_role.identity_pool_unauth_role.arn}"
+  }
+}
