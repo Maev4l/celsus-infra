31309a3afd6d5547ddaccae0a04b36040263a8c4
Maev4l <maeval.nightingale@gmail.com>
2019-12-26 12:25:21 +0100

    Upgrade RDS CA certificate

--- terraform/aws/storage.tf
+++ terraform/aws/storage.tf
@@ -34,6 +34,7 @@ resource "aws_db_instance" "core_storage" {
   instance_class             = "db.t2.micro"
   db_subnet_group_name       = aws_db_subnet_group.core_storage_subnet_group.name
   apply_immediately          = "true"
+  ca_cert_identifier         = "rds-ca-2019"
   availability_zone          = data.aws_availability_zones.available.names[0]
   publicly_accessible        = "true"
   vpc_security_group_ids     = [aws_vpc.main_vpc.default_security_group_id, aws_security_group.core_storage_security_group.id]
