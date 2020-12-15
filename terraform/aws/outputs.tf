9e93a5ba9d92cb7a10793e559c4245630574440d
Maev4l <maeval.nightingale@gmail.com>
2020-12-15 17:49:41 +0100

    Output private registry id

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -101,3 +101,7 @@ output "webClientDistribution" {
 output "imageResizeRepositoryUrl" {
   value = aws_ecr_repository.image_resize_repository.repository_url
 }
+
+output "imagesRegistryId" {
+  value = aws_ecr_repository.image_resize_repository.registry_id
+}
