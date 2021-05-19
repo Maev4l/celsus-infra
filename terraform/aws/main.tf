8adeece3ea584b45a8589769a573c8492ad9d47d
Maev4l <maeval.nightingale@gmail.com>
2021-05-19 16:55:03 +0200

    Add VPC endpoint for S3

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -48,15 +48,17 @@ resource "aws_subnet" "main_vpc_subnet_3" {
   tags = local.tags
 }
 
+// Need this IGW, so the RDS instance is accessible from the internet via dns name
 resource "aws_internet_gateway" "main_vpc_internet_gateway" {
   vpc_id = aws_vpc.main_vpc.id
 
   tags = local.tags
 }
 
-resource "aws_route_table" "main_vpc_route_table" {
+resource "aws_route_table" "vpc_primary_route_table" {
   vpc_id = aws_vpc.main_vpc.id
 
+  // Need this IGW, so the RDS instance is accessible from the internet via dns name
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main_vpc_internet_gateway.id
@@ -65,10 +67,27 @@ resource "aws_route_table" "main_vpc_route_table" {
   tags = local.tags
 }
 
-resource "aws_route_table_association" "main_vpc_route_table_association_1" {
-  subnet_id      = aws_subnet.main_vpc_subnet_1.id
-  route_table_id = aws_route_table.main_vpc_route_table.id
+resource "aws_route_table" "vpc_secondary_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = local.tags
 }
+
+resource "aws_route_table_association" "vpc_primary_route_table_association_1" {
+  subnet_id      = aws_subnet.main_vpc_subnet_1.id
+  route_table_id = aws_route_table.vpc_primary_route_table.id
+}
+
+resource "aws_route_table_association" "vpc_secondary_route_table_association_2" {
+  subnet_id      = aws_subnet.main_vpc_subnet_2.id
+  route_table_id = aws_route_table.vpc_secondary_route_table.id
+}
+
+resource "aws_route_table_association" "vpc_secondary_route_table_association_3" {
+  subnet_id      = aws_subnet.main_vpc_subnet_3.id
+  route_table_id = aws_route_table.vpc_secondary_route_table.id
+}
+
 
 data "template_file" "main_vpc_endpoint_message_queues_policy" {
   template = <<EOF
@@ -95,5 +114,38 @@ resource "aws_vpc_endpoint" "main_vpc_endpoint_message_queues" {
 
   policy = data.template_file.main_vpc_endpoint_message_queues_policy.rendered
 
-  // TODO incoming in future: tags = local.tags
+  tags = local.tags
 }
+
+
+data "template_file" "main_vpc_endpoint_s3_policy" {
+  template = <<EOF
+  {
+  "Version": "2012-10-17",
+  "Statement": [{
+      "Action": ["s3:*"],
+      "Effect": "Allow",
+      "Resource": "*",
+      "Principal": "*"
+   }]
+}
+EOF
+
+}
+
+resource "aws_vpc_endpoint" "main_vpc_endpoint_s3" {
+  vpc_id            = aws_vpc.main_vpc.id
+  vpc_endpoint_type = "Gateway"
+  service_name      = "com.amazonaws.${var.region}.s3"
+
+  policy = data.template_file.main_vpc_endpoint_s3_policy.rendered
+
+  tags = local.tags
+}
+
+
+resource "aws_vpc_endpoint_route_table_association" "subnet2_endpoint_s3" {
+  route_table_id  = aws_route_table.vpc_secondary_route_table.id
+  vpc_endpoint_id = aws_vpc_endpoint.main_vpc_endpoint_s3.id
+}
+
